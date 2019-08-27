e1cd15bad1381a0f2f84e2872b174be2ea17c80e
Andriy Orehov <andriyorehov@gmail.com>
2019-08-27 16:56:58 +0300

    refactor: fix some wemake-python-styleguide messages

--- forkwork/forkwork.py
+++ forkwork/forkwork.py
@@ -1,18 +1,18 @@
-from urllib.parse import urlparse
-from operator import attrgetter
-from collections import namedtuple, OrderedDict
 import calendar
+from collections import OrderedDict, namedtuple
 from datetime import datetime, timedelta
-from email.utils import parsedate, formatdate
+from email.utils import formatdate, parsedate
+from operator import attrgetter
+from urllib.parse import urlparse
 
+import cachecontrol
 import click
 import github3
-import cachecontrol
+import pendulum
 from cachecontrol.caches import FileCache
 from cachecontrol.heuristics import BaseHeuristic
-from tabulate import tabulate
 from halo import Halo
-import pendulum
+from tabulate import tabulate
 
 
 class OneDayHeuristic(BaseHeuristic):
@@ -27,7 +27,7 @@ class OneDayHeuristic(BaseHeuristic):
 
     def warning(self, response):
         msg = "Automatically cached! Response is Stale."
-        return '110 - "%s"' % msg
+        return "110 - {0}".format(msg)
 
 
 @click.group()
@@ -48,7 +48,6 @@ def cli(ctx, url, token):
 
     login, repo = urlparse(url).path[1:].split("/")
     repository = gh.repository(login, repo)
-    cachecontrol.CacheControl(repository.session, cache=FileCache(".fork_work_cache"), heuristic=OneDayHeuristic())
     forks = repository.forks()
 
     spinner.stop()
@@ -62,40 +61,41 @@ def fnm(ctx):
     forks = ctx.obj["forks"]
 
     repo_commits = repository.commits()
-    cachecontrol.CacheControl(repo_commits.session, cache=FileCache(".fork_work_cache"), heuristic=OneDayHeuristic())
-
-    repo_message = [j.message for j in repo_commits]
+    repo_message = [repo_commit.message for repo_commit in repo_commits]
     old_login = ""
     for fork in forks:
         # github api may return nonexistent profile
         try:
-            for c, commit in enumerate(fork.commits(), 1):
+            for index, commit in enumerate(fork.commits(), 1):
                 if commit.message not in repo_message:
                     new_login = fork.owner.login
                     if old_login != fork.owner.login:
-                        print("\n", new_login, fork.html_url)
+                        click.echo("\n", new_login, fork.html_url)
                         old_login = new_login
-                    click.echo(c, commit.message, commit.html_url)
+                    click.echo(index, commit.message, commit.html_url)
         except github3.exceptions.NotFoundError:
-            click.echo("Repository {} not found".format(fork.html_url))
+            click.echo("Repository {0} not found".format(fork.html_url))
 
 
 @cli.command()
-@click.option("--n", default=10, help="Numbers of rows")
+@click.option("--rows", default=10, help="Numbers of rows")
 @click.option("-S", "--star", "sort", flag_value="stargazers_count", default=True, help="Sort by stargazers count")
 @click.option("-F", "--forks", "sort", flag_value="forks_count", help="Sort by forks count")
 @click.option("-I", "--open_issues", "sort", flag_value="open_issues_count", help="Sort by open issues count")
 @click.option("-D", "--updated_at", "sort", flag_value="updated_at", help="Sort by updated at")
 @click.option("-P", "--pushed_at", "sort", flag_value="pushed_at", help="Sort by pushed at")
-@click.option("-W", "--watchers_count", "sort", flag_value="watchers", help="Sort by watchers count (Slow because requires an additional request per fork)")
-@click.option("-C", "--commits", "sort", flag_value="commits", help="Sort by number of commits (Slow because requires an additional requests per fork)")
-@click.option("-B", "--branches", "sort", flag_value="branches", help="Sort by number of branches (Slow because requires an additional request per fork)")
+@click.option("-W", "--watchers_count", "sort", flag_value="watchers", help="Sort by watchers count (Slow because "
+                                                                            "requires an additional request per fork)")
+@click.option("-C", "--commits", "sort", flag_value="commits", help="Sort by number of commits (Slow because requires "
+                                                                    "an additional requests per fork)")
+@click.option("-B", "--branches", "sort", flag_value="branches", help="Sort by number of branches (Slow because "
+                                                                      "requires an additional request per fork)")
 @click.pass_context
-def top(ctx, sort, n):
+def top(ctx, sort, rows):
     repos = []
     forks = ctx.obj["forks"]
     gh = ctx.obj["gh"]
-    d = OrderedDict(
+    columns = OrderedDict(
         [
             ("html_url", "URL"),
             ("stargazers_count", "Stars"),
@@ -103,22 +103,21 @@ def top(ctx, sort, n):
             ("open_issues_count", "Open Issues"),
             ("updated_at", "Last update"),
             ("pushed_at", "Pushed At"),
-        ]
+        ],
     )
-    headers = list(d.values())
+    headers = list(columns.values())
 
     spinner = Halo(text="Fetch information about forks", spinner="dots")
     spinner.start()
 
-    if sort == "branches" or sort == "commits" or sort == "watchers":
-        d[sort] = sort.capitalize()
-        headers.append(d[sort])
-        Repo = namedtuple("Repo", list(d.keys()))
+    if sort in {"branches", "commits", "watchers"}:
+        columns[sort] = sort.capitalize()
+        headers.append(columns[sort])
+        Repo = namedtuple("Repo", list(columns.keys()))
     else:
-        Repo = namedtuple("Repo", list(d.keys()))
+        Repo = namedtuple("Repo", list(columns.keys()))
 
     for fork in forks:
-        cachecontrol.CacheControl(fork.session, cache=FileCache(".fork_work_cache"), heuristic=OneDayHeuristic())
         def_prop = [
             fork.html_url,
             fork.stargazers_count,
@@ -133,26 +132,26 @@ def top(ctx, sort, n):
                 def_prop.append(len(list(fork.branches())))
                 repos.append(Repo(*def_prop))
             except github3.exceptions.NotFoundError:
-                click.echo("\nRepository {} not found".format(fork.html_url))
+                click.echo("\nRepository {0} not found".format(fork.html_url))
         elif sort == "watchers":
             try:
                 repo = gh.repository(fork.owner.login, fork.name)
                 def_prop.append(repo.subscribers_count)
                 repos.append(Repo(*def_prop))
             except github3.exceptions.NotFoundError:
-                click.echo("\nRepository {} not found".format(fork.html_url))
+                click.echo("\nRepository {0} not found".format(fork.html_url))
         elif sort == "commits":
             try:
-                def_prop.append(sum([c.contributions_count for c in fork.contributors()]))
+                def_prop.append(sum((c.contributions_count for c in fork.contributors())))
                 repos.append(Repo(*def_prop))
             except github3.exceptions.NotFoundError:
-                click.echo("\nRepository {} not found".format(fork.html_url))
+                click.echo("\nRepository {0} not found".format(fork.html_url))
         else:
             repos.append(Repo(*def_prop))
 
     sorted_forks = sorted(repos, key=attrgetter(sort), reverse=True)
     humanize_dates_forks = []
-    for fork in sorted_forks[:n]:
+    for fork in sorted_forks[:rows]:
         days_passed_updated_at = (pendulum.now() - pendulum.parse(fork.updated_at)).days
         days_passed_pushed_at = (pendulum.now() - pendulum.parse(fork.pushed_at)).days
         human_updated_at = pendulum.now().subtract(days=days_passed_updated_at).diff_for_humans()
